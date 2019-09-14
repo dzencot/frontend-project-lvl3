c41305e6f313656e749e81a2faf14ea78985316a
Ivan Gagarinov <dzencot@gmail.com>
2019-09-14 12:25:38 +0500

    unusage imports removed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-/* global document Image */
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-console.log('Hello world');
