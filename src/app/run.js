614d5b018819ece784686a99232707b792689c0d
Ivan Gagarinov <dzencot@gmail.com>
2019-09-22 22:00:39 +0500

    save

--- src/app/run.js
+++ src/app/run.js
@@ -5,6 +5,8 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import getLogger from 'webpack-log';
+import validator from 'validator';
+import axios from 'axios';
 
 import Application from './application';
 
@@ -14,9 +16,18 @@ export default () => {
   log('init');
   const input = $(document).find('input');
   const addRssButton = $(document).find('#add-rss');
-  const application = new Application();
+  const application = new Application(axios);
   application.init();
-  input.on('change', (event) => application.onChangeLink(event));
-  log('button:', addRssButton);
+  application.addValidator((data) => validator.isURL(data));
+  input.on('change', (event) => {
+    const link = event.currentTarget.value;
+    const isCurrentLink = application.validateLink(link);
+    if (isCurrentLink) {
+      application.setNewLink(link);
+      input.removeClass('is-invalid');
+    } else {
+      input.addClass('is-invalid');
+    }
+  });
   addRssButton.on('mouseup', (event) => application.onAddRss(event));
 };
